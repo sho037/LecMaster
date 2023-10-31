44ca9d3ba527a61a69a7d2dae316e26be21f2489
sho037 <103867061+sho037@users.noreply.github.com>
2023-10-31 15:46:10 +0900

    Initial Commit

---
+++ src/main/java/shok/lecmaster/LecmasterApplication.java
@@ -0,0 +1,13 @@
+package shok.lecmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LecmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shok/lecmaster/LecmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package shok.lecmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
