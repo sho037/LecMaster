cccd4a3a8838ad4541e9e3927368b846eceb8af7
Shibata1313 <e1b21047@oit.ac.jp>
2023-11-13 20:38:25 +0900

    問題をINSERTできるようにする

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,3 +9,11 @@ CREATE TABLE lecture (
   message VARCHAR,
   password VARCHAR NOT NULL DEFAULT '合言葉'
 );
+
+CREATE TABLE question(
+  id IDENTITY,
+  lecture_id INT,
+  question VARCHAR,
+  answer VARCHAR,
+  FOREIGN KEY (lecture_id) REFERENCES lecture(id)
+);
