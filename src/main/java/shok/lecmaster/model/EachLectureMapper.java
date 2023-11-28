60681e3cc0e6ea44a89801b7b65b9c0e9823274f
Shibata1313 <e1b21047@oit.ac.jp>
2023-11-28 15:07:49 +0900

    CreateEachLecture

---
+++ src/main/java/shok/lecmaster/model/EachLectureMapper.java
@@ -0,0 +1,12 @@
+package shok.lecmaster.model;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+import java.util.ArrayList;
+
+@Mapper
+public interface EachLectureMapper {
+
+}
