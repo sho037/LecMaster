c6a0953cad510f48d4a670a546c8b9b9a23e0b36
Hasegawa891 <e1b21083@oit.ac.jp>
2024-01-09 15:37:15 +0900

    テーブルの初期データを修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO lecture (name) VALUES ('国語');
 INSERT INTO lecture (name) VALUES ('数学');
-INSERT INTO eachLecture (lecture_id,number) VALUES (1,1);
-INSERT INTO eachLecture (lecture_id,number) VALUES (2,1);
+INSERT INTO eachLecture (lecture_id,number,start_date,start_time,end_time) VALUES (1,1,'2024-01-09','13:30:00','15:10:00');
+INSERT INTO eachLecture (lecture_id,number,start_date,start_time,end_time) VALUES (2,1,'2024-01-09','15:20:00','17:00:00');
 INSERT INTO attend (each_lecture_id,name) VALUES (1,'田中 太郎');
 INSERT INTO attend (each_lecture_id,name) VALUES (1,'山田 花子');
 INSERT INTO attend (each_lecture_id,name) VALUES (2,'佐藤 次郎');
